f1e6e2294da6f44976cdb7deccd47d1a7ccf0399
Juan Toledo <toledo+gihub@lazaro.es>
2024-05-17 13:43:05 +0200

    Added flask-sqlalchemy requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "flask",
         "flask-admin",
-        "sqlalchemy",
+        "flask-sqlalchemy",
         "alembic",
         "firebase-admin",
         # other dependencies
