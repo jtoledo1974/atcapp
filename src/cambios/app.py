a694f7d7cff591ca18eeff5351cd0e68df6163ea
Juan Toledo <toledo+github@lazaro.es>
2024-05-19 13:09:25 +0200

    5000 is used on my dev machine.

--- src/cambios/app.py
+++ src/cambios/app.py
@@ -92,4 +92,4 @@ admin.add_view(AdminModelView(User, db.session))
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=5005)
