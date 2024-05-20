1977e3e9379d2cd469a24f7dc830d7d70c9292c4
Juan Toledo <toledo+github@lazaro.es>
2024-05-20 17:15:51 +0200

    Removed unnecessary config module, and increase testability allowing for env configuration.

--- src/cambios/app.py
+++ src/cambios/app.py
@@ -1,16 +1,45 @@
 """Flask App for managing shifts."""
 
+import os
+import secrets
+from pathlib import Path
 from typing import Any
 
 from flask import Flask, Response, redirect, session, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-from .config import load_admin_password, load_key
 from .database import db, init_db
 from .firebase import init_firebase
 from .models import User
 from .routes import register_routes
+
+
+class Config:
+    """Base configuration."""
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI",
+        "sqlite:///shifts.db",
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_urlsafe(32)
+    DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1", "t"]
+    PORT = int(os.getenv("PORT", "5005"))
+
+    @staticmethod
+    def load_key() -> str:
+        """Load the secret key from the environment or .key file."""
+        if "SECRET_KEY" in os.environ:
+            return os.environ["SECRET_KEY"]
+        key_file = Path(".key")
+        if key_file.exists():
+            with key_file.open("r") as f:
+                return f.read().strip()
+        key = secrets.token_urlsafe(32)
+        with key_file.open("w") as f:
+            f.write(key)
+        return key
 
 
 class AdminModelView(ModelView):
@@ -25,12 +54,12 @@ class AdminModelView(ModelView):
         return redirect(url_for("login"))
 
 
-def create_app() -> Flask:
+def create_app(config_class: Config = Config) -> Flask:
     """Create the Flask app."""
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shifts.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = load_key()
+    app.config.from_object(config_class)
+    app.config.from_envvar("APP_SETTINGS", silent=True)
+
     db.init_app(app)
     with app.app_context():
         init_db()
@@ -38,7 +67,6 @@ def create_app() -> Flask:
     init_firebase(app.logger)
     register_routes(app)
 
-    _admin_password = load_admin_password()
     admin = Admin(
         app,
         name="Admin Panel",
