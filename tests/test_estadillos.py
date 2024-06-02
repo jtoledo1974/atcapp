3a5de00089b72169e015b68cbf141eaf70cfff05
Juan Toledo <toledo+github@lazaro.es>
2024-06-02 16:20:42 +0200

    Como los grupos ahora vienen ordenados apropiadamente nos aseguramos de que el test también se verifique tal y como viene en el archivo de prueba.

--- tests/test_estadillos.py
+++ tests/test_estadillos.py
@@ -16,16 +16,10 @@ def test_identifica_grupos(estadillo: Estadillo, preloaded_session: Session) -> 
     grupos = identifica_grupos(estadillo, preloaded_session)
     assert len(grupos) == 4
     # Verifica que hay dos grupos de 8x3, uno de 3x1 y uno de 2x1
-    # No sabemos necesariamente en qué orden vienen los grupos
-    assert any(
-        len(grupo.controladores) == 8 and len(grupo.sectores) == 3 for grupo in grupos
-    )
-    assert any(
-        len(grupo.controladores) == 3 and len(grupo.sectores) == 1 for grupo in grupos
-    )
-    assert any(
-        len(grupo.controladores) == 2 and len(grupo.sectores) == 1 for grupo in grupos
-    )
+    assert len(grupos[0].controladores) == 2
+    assert len(grupos[1].controladores) == 3
+    assert len(grupos[2].controladores) == 8
+    assert len(grupos[3].controladores) == 8
 
     # Comprobar que cada controlador del estadillo está en un grupo o ninguno
     controladores = {periodo.controlador for periodo in estadillo.periodos}
