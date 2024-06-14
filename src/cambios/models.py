19de91b60dcfd68044507bc7b94fc761a013c466
Juan Toledo <toledo+github@lazaro.es>
2024-06-14 15:53:56 +0200

    Metadatos de la base definidos en models.py para que alembic pueda trabajar con ellos.

--- src/cambios/models.py
+++ src/cambios/models.py
@@ -18,13 +18,27 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.orm.decl_api import DeclarativeBase
+from sqlalchemy.schema import MetaData
 
 from . import get_timezone
+
+# Naming conventions for Alembic migrations
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=naming_convention)
 
 
 # Define a base using the declarative base
 class Base(DeclarativeBase):
     """Base class for declarative models."""
+
+    metadata = metadata
 
 
 sectores_estadillo = Table(
